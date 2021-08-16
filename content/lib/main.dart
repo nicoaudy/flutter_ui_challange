4c01943146d13dcb09859d660e764c5ee3683824
Nico Audy <nicoaudy1994@gmail.com>
2021-08-16 08:52:35 +0700

    working on contents app

---
+++ content/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(color: Colors.white),
+    );
+  }
+}
